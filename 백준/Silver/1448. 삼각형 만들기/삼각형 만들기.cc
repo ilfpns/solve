1f7157df4968bc0bb35c5764418ea1c8275d869e
ilfpns <141199911+ilfpns@users.noreply.github.com>
2025-08-26 12:20:22 +0900

    [Silver III] Title: 삼각형 만들기, Time: 156 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.cc
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.cc
@@ -1,40 +1,23 @@
-
 #include <iostream>
-#include <algorithm>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
-int main()
-{
-    int n; // 빨대 개수 변수 생성
-    vector<int> v; 
-
-    cin >> n; 
-
-    // 빨대 길이를 입력받고 벡터에 저장
-    for (int i = 0; i < n; i++) 
-    {
-        int a; 
-        cin >> a; 
-        v.push_back(a);
-    }
-
-    // 벡터 내림차순 정렬
-    sort(v.begin(), v.end(), greater<>());
-
-
-    for (int i = 0; i < n; i++)
-    {
-        if (v[i] < v[i + 1] + v[i + 2])
-        {
-            cout << v[i] + v[i + 1] + v[i + 2] << '\n'; 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n; cin >> n;
+    vector<int> num(n);
+    for (int& i : num) cin >> i;
+    sort(num.begin(), num.end(), greater<>());
+    int cnt = -1;
+    for (int i = 0; i < n - 2; i++) {
+        int x = num[i], y = num[i + 1], z = num[i + 2];
+        if (x < y + z) {
+            cnt = x + y + z;
             break;
         }
-        else if (i == n - 1)
-        {
-            cout << -1 << '\n';
-        }
     }
+    cout << cnt << '\n';
     return 0;
 }
