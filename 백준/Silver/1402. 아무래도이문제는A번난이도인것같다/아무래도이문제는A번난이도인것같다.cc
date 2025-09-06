7f4a400e055fe75c556893e29bcc7fd8b67643b9
ilfpns <141199911+ilfpns@users.noreply.github.com>
2025-09-06 22:32:23 +1200

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	int a = 0; cin >> a;
+	
+	for (int i = 0; i < a; i++) {
+		cout << "yes " << endl;
+
+
+	}
+	return 0;
+}
+
+//sib = (i % 100) * 10;
