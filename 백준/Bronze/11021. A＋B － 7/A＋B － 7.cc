4c81cc9bd91dca537779e17c3f6319e35682d466
ilfpns <141199911+ilfpns@users.noreply.github.com>
2025-09-16 14:01:22 +1200

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long int a; cin >> a;
+
+	for (int i = 1; i < a+1; i++) {
+		int b, q;
+		cin >> b >> q;
+
+		cout << "Case #"<<i<<": " << q + b << '\n';
+	}
+
+	return 0;
+}
