02009878c15e03dbabd9de3f2bde6c03ea5a2356
ilfpns <141199911+ilfpns@users.noreply.github.com>
2025-09-18 22:59:24 +1200

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    
+    for (int i = 0; i < 100; i++) {
+        getline(cin, s);
+        if (!(s.empty())) {
+            cout << s << endl;
+        }
+        s = "";
+    }
+    
+    return 0;
+}
