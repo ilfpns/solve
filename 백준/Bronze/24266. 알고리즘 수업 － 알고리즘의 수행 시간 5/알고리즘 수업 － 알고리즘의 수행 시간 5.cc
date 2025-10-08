8bb523ace6f2a187e764dfc847010710c39d0a8b
ilfpns <141199911+ilfpns@users.noreply.github.com>
2025-10-08 19:45:34 +1300

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <unordered_set>
+#include <deque>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long int a; cin >> a;
+	cout << a*a*a  << '\n' << 3;
+	return 0;
+}
+ 
